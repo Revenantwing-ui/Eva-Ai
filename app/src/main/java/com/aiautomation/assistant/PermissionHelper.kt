cd558393153cfed14721aa4e6eb81dc83caa05a2
Julian Lyman <gumtrident714@gmail.com>
2026-02-08 14:16:42 -0800

    Add files via upload

---
+++ app/src/main/java/com/aiautomation/assistant/PermissionHelper.kt
@@ -0,0 +1,25 @@
+import android.content.Context
+import android.provider.Settings
+import android.text.TextUtils
+import com.aiautomation.assistant.service.AutomationAccessibilityService
+
+object PermissionHelper {
+    fun isAccessibilityServiceEnabled(context: Context): Boolean {
+        val expectedComponentName = "${context.packageName}/${AutomationAccessibilityService::class.java.name}"
+        val enabledServicesSetting = Settings.Secure.getString(
+            context.contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+        ) ?: return false
+
+        val colonSplitter = TextUtils.SimpleStringSplitter(':')
+        colonSplitter.setString(enabledServicesSetting)
+
+        while (colonSplitter.hasNext()) {
+            val componentName = colonSplitter.next()
+            if (componentName.equals(expectedComponentName, ignoreCase = true)) {
+                return true
+            }
+        }
+        return false
+    }
+}
